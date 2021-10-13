3f418170b42ff520b9e0efafe486ff2d859c9e10
jakeengstrom3 <jakeengstrom3@gmail.com>
2021-10-13 13:17:33 -0700

    implement c03. output not working

--- cploration/c03/main.c
+++ cploration/c03/main.c
@@ -1,3 +1,29 @@
-int main(){
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+#define MAX_LINE_LENGTH  200
+int main(int argc, const char *argv[]){
+
+    printf("Usage: %s [filename]\n", argv[0]);
+    exit(EXIT_FAILURE);
+
+    FILE *fin = fopen(argv[1], "r" );
+    perror("Unable to open file!");
+    exit(EXIT_FAILURE);
+
+    char out_file[MAX_LINE_LENGTH];
+    FILE *fout = fopen(out_file, "w+");
+
+    char line[MAX_LINE_LENGTH];
+    unsigned int line_num = 0;
+
+    while(fgets(line, sizeof(line), fin)){
+        line_num++;
+        printf("[Line Num: %u] %s", line_num, line);
+        fprintf(fout, "%s", line);
+    }
+    fclose(fin); fclose(fout);
+
     return 0;
 }
