a7cde2a8b3784732b7f71c6398220e94b48a2886
jakeengstrom3 <jakeengstrom3@gmail.com>
2021-10-14 10:18:36 -0700

    Project 2: implmenet ALU.hdl. ALU-nostat passed, ALU-test failed

--- cploration/c03/main.c
+++ cploration/c03/main.c
@@ -5,22 +5,30 @@
 #define MAX_LINE_LENGTH  200
 int main(int argc, const char *argv[]){
 
-    printf("Usage: %s [filename]\n", argv[0]);
-    exit(EXIT_FAILURE);
+    if(argc != 2){
+        printf("Usage: %s [filename]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
 
     FILE *fin = fopen(argv[1], "r" );
-    perror("Unable to open file!");
-    exit(EXIT_FAILURE);
-
-    char out_file[MAX_LINE_LENGTH];
+    if(fin == NULL){
+        perror("Unable to open file!");
+        exit(EXIT_FAILURE);
+    }
+    
+    char out_file[MAX_LINE_LENGTH] = "";
+    strcat(out_file, argv[1]);
+    strcat(out_file, ".echo");
     FILE *fout = fopen(out_file, "w+");
 
-    char line[MAX_LINE_LENGTH];
+    char line[MAX_LINE_LENGTH] = {0};
     unsigned int line_num = 0;
+    printf("hello");
 
     while(fgets(line, sizeof(line), fin)){
         line_num++;
-        printf("[Line Num: %u] %s", line_num, line);
+        printf("[Line Num: %04d] %s", line_num, line);
         fprintf(fout, "%s", line);
     }
     fclose(fin); fclose(fout);
