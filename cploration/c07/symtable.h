35c298cf67559c5591a3fb5f50b2f2a921c165ea
jakeengstrom3 <jakeengstrom3@gmail.com>
2021-11-14 18:29:07 -0800

    Finish C07

--- cploration/c07/symtable.h
+++ cploration/c07/symtable.h
@@ -1,5 +1,8 @@
 #include "stdint.h"
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define SYMBOL_TABLE_SIZE 100
 
@@ -8,5 +11,12 @@ typedef int16_t hack_addr;
 typedef struct Symbol{
     char *name;
     hack_addr addr;
+}Symbol;
 
-};
+
+struct Symbol* hashArray[SYMBOL_TABLE_SIZE];
+
+int hash(char *str);
+struct Symbol *find(char * key);
+void insert(char* key, hack_addr addr);
+void display_table();
